d658d50730e8c71793a838687e1a8bac20010993
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-17 16:54:59 +0300

    add a routing to the index page.

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,13 @@
 from flask import render_template
+
+from ..request import get_quote
+from ..models import Blog
 from . import main
 
 @main.route("/")
 def index():
-    title = 'Home'
+    blogs = Blog.query.all()
+    quotes = get_quote()
+    title = 'Home: Blog'
     
-    return render_template("index.html", title=title)
+    return render_template("index.html", title=title, blogs=blogs, quotes=quotes)
