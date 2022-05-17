f96fa9d19fed1c799e5f3f3060dd258dad8a4c85
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-17 17:07:17 +0300

    create a blogform

---
+++ app/main/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import InputRequired
+
+class BlogForm(FlaskForm):
+    title = StringField("Blog Title", validators=InputRequired)
+    blog = TextAreaField('Share Your Blog', validators=[InputRequired()])
+    submit = SubmitField('Post')
+    
