47c20b4a43385f9050a4f98f6e86ddc1b8d1af12
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-17 20:31:20 +0300

    add a routing for comments form

--- app/main/views.py
+++ app/main/views.py
@@ -2,8 +2,8 @@ from flask import render_template, redirect, url_for
 from flask_login import current_user, login_required
 from . import main
 from ..request import get_quote
-from .forms import BlogForm
-from ..models import Blog
+from .forms import BlogForm, CommentForm
+from ..models import Blog, Comment
 
 
 @main.route("/")
@@ -14,7 +14,7 @@ def index():
     
     return render_template("index.html", title=title, blogs=blogs, quotes=quotes)
 
-@main.route("/", methods=['GET', 'POST'])
+@main.route("/pitch/new-pitch", methods=['GET', 'POST'])
 @login_required
 def add_blog():
     form = BlogForm()
@@ -26,4 +26,20 @@ def add_blog():
         new_blog= Blog(category=category, title=title,blog=blog, user=user_id)
         new_blog.save_blog()
         return redirect(url_for('main.index'))
-    return render_template('index.html', form=form)
+    title = 'Create New Pitch'
+    return render_template('index.html', title=title, form=form)
+
+@main.route('/comment/<int:blog_id>', methods=['GET', 'POST'])
+@login_required
+def comment(blog_id):
+    form = CommentForm()
+    blog = Blog.query.get(blog_id)
+    comments = Comment.query.filter_by(blog_id=blog_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data
+        blog_id = blog.id
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(comment=comment, user_id =user_id, blog_id =blog_id)      
+        new_comment.save_comment()
+        return redirect(url_for('.comment', blog_id =blog_id))
+    return render_template('comment.html', form=form, blog=blog, comments=comments)
