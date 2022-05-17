c60c0b431edcbe11992c126ba51bfc0d655aee7a
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-17 18:14:30 +0300

    define migrate command in the shell context

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 from flask_script import Manager, Server
-from app import create_app
+from app import create_app,db
+from flask_migrate import Migrate, MigrateCommand
+from app.models import Comment, Blog, User
 
 app = create_app('development')
 
@@ -9,7 +11,18 @@ manager.add_command('server', Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, db=db, User=User,Blog=Blog, Comment=Comment)
+
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    """Run the unit tests"""
+    import unittest
+
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__=="__main__":
     manager.run()
